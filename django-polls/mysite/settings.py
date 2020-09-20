fccf261528f1a5c5265afc22183268ba653598a7
hjet <hjet@users.noreply.github.com>
2020-09-20 19:24:58 -0400

    Update package versions and disable query param auth

--- django-polls/mysite/settings.py
+++ django-polls/mysite/settings.py
@@ -133,6 +133,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
