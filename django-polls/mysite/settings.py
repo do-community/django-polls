d6522097ac2fa71422d2ef4f2bbd52d201bd47f6
Aptha Gowda <iamuraptha@gmail.com>
2021-04-06 00:32:10 +0530

    update settings.py for debug=True

--- django-polls/mysite/settings.py
+++ django-polls/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv('DEBUG', False)=='True'
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
